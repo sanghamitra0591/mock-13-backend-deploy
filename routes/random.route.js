6d40bf56a55593db3f1c3c8cdb0862a9c9ae9650
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-03-02 15:10:17 +0530

    initialized

--- routes/random.route.js
+++ routes/random.route.js
@@ -3,9 +3,15 @@ const express= require("express");
 const randomRouter= express.Router();
 
 randomRouter.get("/", (req, res)=>{
-    const length= Math.random();
-    console.log(length);
-    res.send("apple");
+    const length= Math.ceil(Math.random()*6);
+    const arr= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+     "s", 't', 'u', "v", "w", 'x', 'y', 'z'];
+    let random="";
+    for(let i=0;i<length;i++){
+        const no= Math.ceil(Math.random()*26);
+        random+=arr[no];
+    }
+    res.send(random);
 })
 
 
